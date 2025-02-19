3a1e0f6fd3d6ea1dcb63860170d64f9185a0f65f
Somnath Banerjee <somnathbanerjee342000@gmail.com>
2025-02-19 10:49:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -67,21 +67,7 @@ st.write('''Welcome to the **movie exploration**website. Here, I have **4808** m
 st.write('''My Other Projects :
             Retail Techstore Sales Analysis (Please Go through the link) : https://salesanalytics-somnath-techstore.streamlit.app/''')
 
-# List of the first 6 images for the slideshow
-image_paths_first_6 = [
-    "cinemaimage04.jpg",
-    "cinemaimage06.jpg",
-    "cinemaimage07.jpg",
-    "cinemaimage02.jpg",
-    "cinemaimage08.jpg",
-    "cinemaimage05.jpg"
-]
-
-# Create a slideshow for the first 6 images with a 2-second interval
-placeholder_1 = st.empty()  # Empty placeholder to refresh the image
-for i in range(len(image_paths_first_6)):
-    placeholder_1.image(image_paths_first_6[i], caption="Explore the Magic of Cinema", use_container_width=True)
-    time.sleep(7)  # Wait for 7 seconds before displaying the next image
+st.image('cinemaimage08.jpg', caption='Welcome to sales dashboard & forcating', use_container_width=True)
 
 # -------------------------------------------------------------------------------------------------------------------
 
@@ -130,4 +116,19 @@ Cinema encompasses a wide variety of genres, each with its own conventions and s
 5. **Science Fiction**: Explores speculative concepts, often involving futuristic technology, space travel, and alternate realities.  
 6. **Documentary**: Non-fiction films that depict real-life events, people, and situations, often with a focus on education or social issues.
 ''')
+
+# List of the first 6 images for the slideshow
+image_paths_first_6 = [
+    "cinemaimage04.jpg",
+    "cinemaimage06.jpg",
+    "cinemaimage07.jpg",
+    "cinemaimage02.jpg",
+    "cinemaimage05.jpg"
+]
+
+# Create a slideshow for the first 6 images with a 2-second interval
+placeholder_1 = st.empty()  # Empty placeholder to refresh the image
+for i in range(len(image_paths_first_6)):
+    placeholder_1.image(image_paths_first_6[i], caption="Explore the Magic of Cinema", use_container_width=True)
+    time.sleep(7)  # Wait for 7 seconds before displaying the next image
 
