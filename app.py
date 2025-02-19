995e87b1db0da8f93aa39d5c95cd212d86a2df33
Somnath Banerjee <somnathbanerjee342000@gmail.com>
2025-02-19 10:28:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pandas as pd
 import pickle
 import requests
-import sqlite3
 import time
 
 
@@ -68,17 +67,19 @@ st.write('''Welcome to the **movie exploration**website. Here, I have **4808** m
 st.write('''My Other Projects :
             Retail Techstore Sales Analysis (Please Go through the link) : https://salesanalytics-somnath-techstore.streamlit.app/''')
 
-# List of the first 4 images for the slideshow
-image_paths_first_4 = [
+# List of the first 6 images for the slideshow
+image_paths_first_6 = [
     "cinemaimage04.jpg",
     "cinemaimage06.jpg",
     "cinemaimage07.jpg",
-    "cinemaimage08.jpg"
+    "cinemaimage02.jpg",
+    "cinemaimage08.jpg",
+    "cinemaimage05.jpg"
 ]
 
-# Create a slideshow for the first 4 images with a 2-second interval
+# Create a slideshow for the first 6 images with a 2-second interval
 placeholder_1 = st.empty()  # Empty placeholder to refresh the image
-for i in range(len(image_paths_first_4)):
+for i in range(len(image_paths_first_6)):
     placeholder_1.image(image_paths_first_4[i], caption="Explore the Magic of Cinema", use_container_width=True)
     time.sleep(7)  # Wait for 7 seconds before displaying the next image
 
@@ -129,28 +130,4 @@ Cinema encompasses a wide variety of genres, each with its own conventions and s
 5. **Science Fiction**: Explores speculative concepts, often involving futuristic technology, space travel, and alternate realities.  
 6. **Documentary**: Non-fiction films that depict real-life events, people, and situations, often with a focus on education or social issues.
 ''')
-
-# -------------------------------------------------------------------------------------------------------------------
-
-# Last batch of images for slideshow
-image_paths_last_4 = [
-    "cinemaimage01.jpg",
-    "cinemaimage02.jpg",
-    "cinemaimage03.jpg",
-    "cinemaimage05.jpg"
-]
-
-# Create a slideshow for the last 4 images with a 2-second interval
-placeholder_2 = st.empty()  # Empty placeholder for second slideshow
-for i in range(len(image_paths_last_4)):
-    placeholder_2.image(image_paths_last_4[i], caption="Explore the Magic of Cinema", use_container_width=True)
-    time.sleep(7)  # Wait for 2 seconds before displaying the next image
-
-
-# -------------------------------------------------------------------------------------------------------------------
-
-# Display Information about Hollywood
-st.write('''Hollywood is a district in Los Angeles, California, known as the heart of the American film industry. 
-It's where many major movie studios are based, and it's famous worldwide as the center of entertainment production.
-This Code is written by Somnath Banerjee. Mail : somnathbanerjee342000@gmail.com''')
 
